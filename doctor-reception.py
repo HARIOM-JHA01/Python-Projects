148c87f1202f1081b00b3756274b870c60d5c566
HARIOM-JHA01 <96457683+HARIOM-JHA01@users.noreply.github.com>
2022-10-08 09:50:25 +0530

    created doctor-reception.py
    This program will work as automation for displaying message to patient to come for treatment and a single button for doctor to close the day.

---
+++ doctor-reception.py
@@ -0,0 +1,8 @@
+n = 1
+c = 1
+while(c!=0):
+    print("Patient no.",n,"Come in.")
+    print("Would you like to continue : ")
+    c = int(input("Enter (0/1) (exit/continue) : "))
+    n = n+1
+print("Thank you for the time and Have a nice day.")
